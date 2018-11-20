2872969c51b7c6bd416116f166b49d69e837acbc
East <eastmaels@gmail.com>
2018-11-21 00:24:16 +0900

    added handling to return servo to home degrees

--- index.js
+++ index.js
@@ -14,17 +14,40 @@ const stream = client.blockchain.getOperationsStream()
 var five = require("johnny-five");
 var board = new five.Board({ port: "COM6" });
 
+const MS_TO_COMPLETE = 3000
+const MS_TO_COMPLETE_WITH_BUFFER = 3500
+const HOME_DEGREES = 0
+const TO_DEGREES = 180
+
+function handler() {
+  console.log('move complete')
+}
+
+function returnToHome(servo){
+  if(servo.value === TO_DEGREES) {
+    servo.to(HOME_DEGREES, MS_TO_COMPLETE);
+  }
+}
+
 board.on("ready", function() {
-  var servo = new five.Servo(9);
+  var servo = new five.Servo({
+    pin: 9,
+    startAt: 0
+  });
+  servo.on('move:complete', handler)
+
+  board.repl.inject({
+    servo: servo
+  });
 
   // Stream Steem Blockchain
   stream.on('data', async operation => {
     // Look for comment type of transaction
     if (operation.op[0] == 'transfer') {
-      console.log('is transfer operation: ', operation.op[1])
       if (operation.op[1].to == ACCOUNT_NAME) {
         console.log("received transfer to: ", ACCOUNT_NAME)
-        servo.sweep();
+        servo.to(TO_DEGREES, MS_TO_COMPLETE);
+        setTimeout(returnToHome.bind(null, servo), MS_TO_COMPLETE_WITH_BUFFER);
       }
     }
   })  // end: stream.on()
